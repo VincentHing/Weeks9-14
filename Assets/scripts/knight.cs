9ebc235576d478fe7e3589fdc8ef2e29b8e55662
hingley <hingley@OA-S141-25>
2026-03-24 09:38:15 -0400

    learned animation events

---
+++ Assets/scripts/knight.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class knight : MonoBehaviour
+{
+    public AudioSource SFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void footstep()
+    {
+        SFX.Play();
+    }
+}

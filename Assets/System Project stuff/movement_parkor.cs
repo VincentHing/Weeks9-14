75179fcc963b650897ebfe898c06482993fc4eca
hingley <hingley@OA-S213-05>
2026-03-27 12:39:05 -0400

    setup the frameworks for actions, will branch after this

--- Assets/System Project stuff/movement_parkor.cs
+++ Assets/System Project stuff/movement_parkor.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class movement_parkor : MonoBehaviour
 {
     public float speed=5;
     public Vector2 movementModif;
     public bool animPlaying=false;
+    UnityEvent Parkors = new UnityEvent();
 
     //temporarily holds the transform so i can mess around with it easier
     public Vector3 position;
@@ -13,7 +15,7 @@ public class movement_parkor : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Parkors.AddListener(ParkorAction);
     }
 
     // Update is called once per frame
@@ -48,10 +50,40 @@ public class movement_parkor : MonoBehaviour
         transform.position = position;
 
     }
+    
     //this and update were mostly copied from the input system follow along in class
     public void OnMove(InputAction.CallbackContext context)
     {
-        movementModif = context.ReadValue<Vector2>();
+        if (context.performed==true)
+        {
+            movementModif = context.ReadValue<Vector2>();
+        }
+       
     }
 
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed==true)
+        {
+            Parkors.Invoke();
+        }
+
+    }
+
+    public void ParkorAction()
+    {
+        if (!animPlaying)
+        {
+            animPlaying = true;
+            //are you within walclimb range
+            if(position.x <= 1.48f || position.x > 1f)
+            {
+                //call a walclimb function
+            }
+            else
+            {
+                //call the backflip function
+            }
+        }
+    }
 }

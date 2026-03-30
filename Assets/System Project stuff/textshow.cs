7e558c3833f97f083c6e9c68be84b8a40b65e5c5
hingley <hingley@OA-S213-10>
2026-03-30 12:50:43 -0400

    adjusted parkore text time visible

--- Assets/System Project stuff/textshow.cs
+++ Assets/System Project stuff/textshow.cs
@@ -5,7 +5,7 @@ public class textshow : MonoBehaviour
     //positions for being on screen and off screen, shown here for clarity
     Vector3 onScreen = new Vector3(0, 3.72f, 0);
     Vector3 offScreen = new Vector3(0, 7.72f, 0);
-    public float clock = 1.5f;
+    public float clock = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,7 +19,7 @@ public class textshow : MonoBehaviour
         //make time move
        clock += Time.deltaTime;
         //if it's outside the alloted time it's off screen, if not it's on
-        if (clock > 1.5f)
+        if (clock > 0.5f)
         {
             transform.position = offScreen;
         }
